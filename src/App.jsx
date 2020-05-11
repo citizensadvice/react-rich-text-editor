44d1737a76578691b9516110fcfff6bc3db05ae5
Chris Brett <c.brett84@gmail.com>
2020-05-11 12:43:38 +0100

    Fix broken application render when clicking RTE.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import React, { useState } from 'react';
-import Html from 'slate-html-serializer';
+import React from 'react';
 import RichTextEditor from './RichTextEditor';
 import './index.scss';
-import { rules } from './RichTextEditor/utils';
 
-const html = new Html({ rules });
-const isInvalid = false;        
+const isInvalid = false;
 
 export function App() {
-  const [contents, setContents] = useState();
-
   return (
-    <>
-      <RichTextEditor
-        isInvalid={isInvalid}
-        onEditorChange={(value) => setContents(html.serialize(value))}
-      />
-
-      <p>
-        <label htmlFor="output">
-          Current value
-        </label>
-        <output>
-          <pre>
-            {contents}
-          </pre>
-        </output>
-      </p>
-    </>
+    <RichTextEditor
+      isInvalid={isInvalid}
+    />
   );
 }
